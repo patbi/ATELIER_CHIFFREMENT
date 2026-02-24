fd2bae3eace1c866b86e8f326cd5c74c7354daa1
Patrick BIYAGA <biyagapatrick@gmail.com>
2026-02-24 11:33:00 +0100

    Add encryption and decryption example with NaCl
    Implement secret box encryption and decryption using NaCl.

--- app/secretbox_crypto.py
+++ app/secretbox_crypto.py
@@ -1,1 +1,24 @@
+from nacl.secret import SecretBox
+from nacl.utils import random
+import base64
 
+# Generate key (32 bytes)
+key = random(SecretBox.KEY_SIZE)
+
+# Save key in base64
+print("Key (save this):", base64.b64encode(key).decode())
+
+box = SecretBox(key)
+
+# Message
+message = b"Top secret message"
+
+# Encrypt
+encrypted = box.encrypt(message)
+
+print("Encrypted:", encrypted)
+
+# Decrypt
+decrypted = box.decrypt(encrypted)
+
+print("Decrypted:", decrypted.decode())
