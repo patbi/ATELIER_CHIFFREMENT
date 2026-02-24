dd58ab9c751d5e923249b141a00f6259942b36d1
Patrick BIYAGA <biyagapatrick@gmail.com>
2026-02-24 11:21:07 +0100

    Implement Fernet encryption and decryption
    Added Fernet encryption and decryption example using environment variable for key.

--- app/fernet_atelier1.py
+++ app/fernet_atelier1.py
@@ -1,1 +1,23 @@
+import os
+from cryptography.fernet import Fernet
 
+# Get key from GitHub Secret
+key = os.environ.get("FERNET_KEY")
+
+if not key:
+    raise Exception("FERNET_KEY not found in environment variables")
+
+key = key.encode()
+
+fernet = Fernet(key)
+
+# Encrypt
+message = b"Secret message from Atelier 1"
+encrypted = fernet.encrypt(message)
+
+print("Encrypted:", encrypted)
+
+# Decrypt
+decrypted = fernet.decrypt(encrypted)
+
+print("Decrypted:", decrypted.decode())
